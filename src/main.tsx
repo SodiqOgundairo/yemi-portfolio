2a1ea9a1a23f508cee1b078429e54f9cf17119d5
Sodiq Ogundairo <kingmclamba1@gmail.com>
2025-06-28 13:12:18 +0100

    tailwindcss adjusted

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import AOS from 'aos'
 import 'aos/dist/aos.css' // AOS styles
-import './index.css'     // Tailwind styles (should be after AOS to allow overrides if needed, though usually not an issue)
+import './assets/css/index.css'     // Tailwind styles (should be after AOS to allow overrides if needed, though usually not an issue)
 
 AOS.init({
   duration: 1000, // values from 0 to 3000, with step 50ms
